9070a18d9b1054355216153eacc68283e06ed44c
Thiago Paes <mrprompt@gmail.com>
2018-03-13 13:57:57 -0300

    feature(OTA): Add wifi update with OTA

--- estufa-iot.ino
+++ estufa-iot.ino
@@ -1,4 +1,7 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <DHT.h>
 
 //defines
@@ -55,7 +58,8 @@ void connect(void)
     Serial.println("- Conectando-se à rede WiFi...");
     
     delay(1000);
-    
+
+    WiFi.mode(WIFI_STA);
     WiFi.begin(SSID_REDE, SENHA_REDE);
 
     while (WiFi.status() != WL_CONNECTED) 
@@ -63,6 +67,39 @@ void connect(void)
         delay(500);
         Serial.print(".");
     }
+
+    // Porta padrao do ESP8266 para OTA eh 8266 - Voce pode mudar ser quiser, mas deixe indicado!
+    // ArduinoOTA.setPort(8266);
+   
+    // O Hostname padrao eh esp8266-[ChipID], mas voce pode mudar com essa funcao
+    // ArduinoOTA.setHostname("nome_do_meu_esp8266");
+   
+    // Nenhuma senha eh pedida, mas voce pode dar mais seguranca pedindo uma senha pra gravar
+    // ArduinoOTA.setPassword((const char *)"123");
+   
+    ArduinoOTA.onStart([]() {
+      Serial.println("- Inicio...");
+    });
+    
+    ArduinoOTA.onEnd([]() {
+      Serial.println("- Fim!");
+    });
+    
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("- Progresso: %u%%r", (progress / (total / 100)));
+    });
+    
+    ArduinoOTA.onError([](ota_error_t error) {
+      Serial.printf("- Erro [%u]: ", error);
+      
+      if (error == OTA_AUTH_ERROR) Serial.println("Autenticacao Falhou");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Falha no Inicio");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Falha na Conexao");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Falha na Recepcao");
+      else if (error == OTA_END_ERROR) Serial.println("Falha no Fim");
+    });
+    
+    ArduinoOTA.begin();
 
     Serial.println("");
     Serial.println("- WiFi connectado com sucesso!");  
@@ -100,6 +137,8 @@ void loop()
     int Umidade;
     int Temperatura;
     char Fields[40];
+
+    ArduinoOTA.handle();
     
     //Força desconexão ao ThingSpeak (se ainda estiver desconectado)
     if (client.connected())
